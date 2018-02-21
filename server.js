76faf651ec6fda133a5a3d92e1bd828e46a9d476
Adam Cooper <adam.cooper@galvanize.com>
2018-02-21 16:13:19 -0500

    Hooked in knex. Stubbed out routes. Filled in GET /coffees route.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const knex = require('./db');
 const PORT = process.env.PORT || 8000;
 
 // Middleware
@@ -13,6 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   response.send('The slash route is working, so you better start working.');
 });
+
+app.get('/coffees', (request, response) => {
+  knex('coffees')
+  .then( rows => response.json(rows));
+});
+
+app.get('/coffees/:id', (request, response) => {});
+
+app.post('/coffees', (request, response) => {});
+
+app.put('/coffees/:id', (request, response) => {});
+
+app.delete('/coffees/:id', (request, response) => {});
 
 // Listening
 app.listen( PORT, () => {
