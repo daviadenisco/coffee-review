661ae2df92ac351b047ace64d5cc2686fcf53450
Davia DeNisco <davia622@gmail.com>
2018-02-22 11:34:38 -0500

    added patch and delete routes

--- server.js
+++ server.js
@@ -16,12 +16,26 @@ app.get('/', (request, response) => {
 });
 
 app.get('/coffees', (request, response) => {
+  // this returns an array of objects representing all the rows in that table (and all the columns in those rows)
+  // SQL equivalent of (syntax might be wrong) SELECT * FROM coffees
   knex('coffees')
   .then( rows => response.json(rows));
 });
 
-app.get('/coffees/:id', (request, response) => {});
+app.get('/coffees/:id', (request, response) => {
+  // access the request object, tack on params, then id
+  const coffeeId = request.params.id;
+  knex('coffees')
+  .where('id', coffeeId) // SELECT * FROM coffees WHERE id=coffeeId;
+  .then(rows => {
+    // only one thing in the rows array, and we want the first thing in the array, which is the id, so rows[0] is what we want
+    const foundCoffee = rows[0];
+    // send the response as json
+    response.json(foundCoffee);
+  })
+});
 
+// http --form POST localhost:8000/coffees origin=Mexico roast=medium price=13.99 flavor=nutty
 app.post('/coffees', (request, response) => {
   // origin, flavor, roast, price
   console.log(`request.body: ${request.body}`); // debug
@@ -37,11 +51,36 @@ app.post('/coffees', (request, response) => {
   });
 });
 
-app.put('/coffees/:id', (request, response) => {});
+// echo '{"price": "15.99"}' | http PATCH localhost:8000/coffees/1
+app.patch('/coffees/:id', (request, response) => {
+  const coffeeId = request.params.id;
+  const {origin, flavor, roast, price} = request.body;
 
-app.delete('/coffees/:id', (request, response) => {});
+  knex('coffees')
+  .where('id', coffeeId)
+  .returning('*')
+  .update({origin, flavor, roast, price})
+  .then (rows => {
+    const coffee = rows[0];
+    response.json(coffee);
+  });
+});
+
+app.put('/coffees/:id', (request, response) => {
+
+});
+
+app.delete('/coffees/:id', (request, response) => {
+  const coffeeId = request.params.id;
+  knex('coffees')
+  .where('id', coffeeId)
+  .del()
+  .then(response.send('Deleted'));
+});
 
 // Listening
+// takes two arguments, the PORT variable and an anonymous function
+
 app.listen( PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 })
